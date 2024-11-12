c15f59d5be83f35d70194a8731cbbcbe9b6b216c
himani zambare <himanizambare3@gmail.com>
2024-11-12 17:06:06 +0530

    Add deployment scripts for GitHub Pages

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -6,7 +6,7 @@ import hero_image_back from "../../assets/hero_image_back.png";
 import Heart from "../../assets/heart.png";
 import Calories from "../../assets/calories.png";
 import {motion} from 'framer-motion';
-import { type } from "@testing-library/user-event/dist/type";
+
 
 const Hero = () => {
   const transition={type:'spring',duration:3};
